afae67f405d688ec437105c8edfcf732e9a1c7af
Parham Alvani <parham.alvani@gmail.com>
2015-05-09 00:31:19 +0430

    types sample code is added.

---
+++ src/types.py
@@ -0,0 +1,16 @@
+# In The Name Of God
+# ========================================
+# [] File Name : types.py
+#
+# [] Creation Date : 09-05-2015
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+__author__ = 'Parham Alvani'
+
+number = 10
+print("number: %d" % number)
+number = 10.1
+print("number: %g" % number)
+number = 1j
+print("number * number: %d" % (number * number).real)
