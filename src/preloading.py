373efce318753d769edfe18504beb872bc73bcfe
Parham Alvani <parham.alvani@gmail.com>
2015-05-09 21:12:01 +0430

    sample codes and slides improved.

---
+++ src/preloading.py
@@ -0,0 +1,9 @@
+# In The Name Of God
+# ========================================
+# [] File Name : preloading.py
+#
+# [] Creation Date : 09-05-2015
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+__author__ = 'Parham Alvani'
--- src/types.py
+++ src/types.py
@@ -15,6 +15,11 @@ print("number: %g" % number)
 number = 1j
 print("number * number: %d" % (number * number).real)
 
+string_one = 'parham\n'
+string_two = "parham\n"
+print(string_one)
+print(string_two)
+
 my_set = {1, 2, 3, "parham"}
 my_list = [1, 2, 3, 'parham']
 my_tuple = (1, 2, 10, "python")
@@ -22,3 +27,9 @@ my_tuple = (1, 2, 10, "python")
 print(my_set)
 print(my_list)
 print(my_tuple)
+
+my_set.add(10)
+print(my_set)
+
+print(my_list[0])
+print(my_list[-1])
