76b802b956a45f052d0eae577ddbb007c19d52cb
Parham Alvani <parham.alvani@gmail.com>
2015-05-10 07:42:41 +0430

    generator sample code improved.

--- src/generator.py
+++ src/generator.py
@@ -16,4 +16,21 @@ print(min(lst))
 print(min(lst))
 print(min(gen))
 # we will got error on this :-)
-print(min(gen))
+# print(min(gen))
+
+
+def fibonacci(count):
+    # These seed values generate 0 and 1 when fed into the loop
+    a, b = -1, 1
+    while count > 0:
+        # Yield the value for this iteration
+        c = a + b
+        yield c
+        # Update values for next iteration
+        a, b = b, c
+        count -= 1
+
+
+print("Fibonacci sequence: ")
+for x in fibonacci(10):
+    print(x)
