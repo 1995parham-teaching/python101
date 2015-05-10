ef8409ecb193956b1b68930a387539104fd6252f
Parham Alvani <parham.alvani@gmail.com>
2015-05-10 09:24:16 +0430

    functions sample code is added.

---
+++ src/functions.py
@@ -0,0 +1,16 @@
+# In The Name Of God
+# ========================================
+# [] File Name : functions.py
+#
+# [] Creation Date : 10-05-2015
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+__author__ = 'Parham Alvani'
+
+
+def square(x):
+    return x * x
+
+
+print(square(10))
