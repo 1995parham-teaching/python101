f45672e23887fb80e30169b7c85bceb7c468d2e4
Parham Alvani <parham.alvani@gmail.com>
2015-05-12 05:16:46 +0430

    overloading sample code is added.

---
+++ src/overloading.py
@@ -0,0 +1,25 @@
+# In The Name Of God
+# ========================================
+# [] File Name : overloading.py
+#
+# [] Creation Date : 12-05-2015
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+__author__ = 'Parham Alvani'
+
+
+class Hello:
+    def __init__(self):
+        print("Hello created")
+
+    def __str__(self):
+        return "Hello"
+
+    def __int__(self):
+        return 110
+
+
+h = Hello()
+print(str(h))
+print(int(h))
