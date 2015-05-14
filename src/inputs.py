7662fd3f64a0fe2598e052ba6865911f0bb0fa99
Parham Alvani <parham.alvani@gmail.com>
2015-05-14 16:20:03 +0430

    inputs sample code and slides is added.

--- src/inputs.py
+++ src/inputs.py
@@ -7,3 +7,6 @@
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
 __author__ = 'Parham Alvani'
+
+name = input("Please enter your name: ")
+print("Hello %s" % name)
