1b6d598eb6b8ad47eb1fb147ec93dae68d6aebdd
Parham Alvani <parham.alvani@gmail.com>
2015-05-15 00:43:16 +0430

    echoServer sample code is added.

--- src/echoServer.py
+++ src/echoServer.py
@@ -7,3 +7,21 @@
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
 __author__ = 'Parham Alvani'
+
+import socket
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+server.bind(('', 1373))
+server.listen(5)
+run = True
+
+while run:
+    client, client_address = server.accept()
+    print("Connection from %s:%d" % (client_address[0], client_address[1]))
+    while True:
+        data = client.recv(1024)
+        if not data:
+            break
+        client.send(data)
+    client.close()
