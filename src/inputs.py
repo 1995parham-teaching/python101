3f5d4b73c05bd2e32c7bbd18aef157119777ac55
Parham Alvani <parham.alvani@gmail.com>
2015-05-14 16:36:35 +0430

    inputs sample code improved.

--- src/inputs.py
+++ src/inputs.py
@@ -10,3 +10,11 @@ __author__ = 'Parham Alvani'
 
 name = input("Please enter your name: ")
 print("Hello %s" % name)
+
+number = input("Please enter number: ")
+try:
+    number = int(number)
+except ValueError:
+    print("%s is not number" % number)
+else:
+    print("Your number is %d" % number)
