6e9aca05e46d3bb9fe53a3c0a0c8dd0761eb935a
Parham Alvani <parham.alvani@gmail.com>
2015-05-17 00:20:14 +0430

    Overloading sample code improved.

--- src/overloading.py
+++ src/overloading.py
@@ -19,7 +19,12 @@ class Hello:
     def __int__(self):
         return 110
 
+    def __add__(self, other):
+        return other
+
 
 h = Hello()
 print(str(h))
 print(int(h))
+print(h + 10)
+print(10 + h)
