767eabdae089abb2a462a84d827413efa49b724b
Parham Alvani <parham.alvani@gmail.com>
2015-05-22 23:47:17 +0430

    int sample code is added.

---
+++ src/int.py
@@ -0,0 +1,15 @@
+# In The Name Of God
+# ========================================
+# [] File Name : int.py
+#
+# [] Creation Date : 22-05-2015
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+__author__ = 'Parham Alvani'
+
+A = 10
+print(bin(A))
+print(hex(A))
+print(A.to_bytes(2, byteorder='little'))
+print(A.to_bytes(2, byteorder='big'))
