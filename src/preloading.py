0f56dbb89e9d1e02b9547104965a774862efd4cd
Parham Alvani <parham.alvani@gmail.com>
2015-06-01 00:14:23 +0430

    preloading sample code completed.

--- src/preloading.py
+++ src/preloading.py
@@ -7,3 +7,15 @@
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
 __author__ = 'Parham Alvani'
+
+import functools
+
+
+def main_function(arg1, arg2, arg3):
+    print("arg1 : " + str(arg1))
+    print("arg2 : " + str(arg2))
+    print("arg3 : " + str(arg3))
+
+
+partial_function = functools.partial(main_function, arg3=10)
+partial_function(20, 30)
