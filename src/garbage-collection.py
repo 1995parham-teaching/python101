c3c4928dcc355ffa483992a903b72e947daf1281
Parham Alvani <parham.alvani@gmail.com>
2015-06-02 23:02:13 +0430

    garbage collector sample is added.

---
+++ src/garbage-collection.py
@@ -0,0 +1,16 @@
+# In The Name Of God
+# ========================================
+# [] File Name : garbage-collection.py
+#
+# [] Creation Date : 02-06-2015
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+__author__ = 'Parham Alvani'
+
+x = 10
+print(id(x))
+x = "foo"
+print(id(x))
+x = None
+print(id(x))
