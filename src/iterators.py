6693b4f03095129384ebcc29a872b1d39e2d1886
Parham Alvani <parham.alvani@gmail.com>
2015-06-02 05:23:14 +0430

    iterators sample code improved.

--- src/iterators.py
+++ src/iterators.py
@@ -68,3 +68,23 @@ z = ZRange(5)
 
 print(list(z))
 print(list(z))
+
+
+class Range:
+    def __init__(self, max):
+        self.max = max
+
+    def __iter__(self):
+        for x in range(self.max):
+            yield x
+
+    def __len__(self):
+        return self.max
+
+    def __reversed__(self):
+        for x in range(self.max - 1, -1, -1):
+            yield x
+
+
+print(list(Range(4)))
+print(list(reversed(Range(4))))
