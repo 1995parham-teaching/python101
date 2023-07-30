dcc2f463b04e7b687ca2e7004d41814d04942351
Parham Alvani <parham.alvani@gmail.com>
2023-07-30 15:47:10 +0330

    feat: improve introduction presentation

---
+++ presentation/introduction/src/secret_counter.py
@@ -0,0 +1,20 @@
+class SecretCounter:
+    __secret_count = 0
+
+    def inc(self):
+        self.__secret_count += 1
+        print(f"secret counter increased to: {self.__secret_count}")
+
+
+if __name__ == "__main__":
+    counter = SecretCounter()
+    counter.inc()
+    counter.inc()
+
+    # AttributeError: 'SecretCounter' object has no attribute '__secret_count'
+    # print(counter.__secret_count)
+
+    print(
+        "access to hidden property by force: "
+        f"{counter._SecretCounter__secret_count}"  # type: ignore
+    )
