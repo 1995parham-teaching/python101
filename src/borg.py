efda8a941dd2a5a99e1fd8e53dffd39207209eb5
Parham Alvani <parham.alvani@gmail.com>
2023-08-28 02:35:01 -0400

    feat: update borg example

--- src/borg.py
+++ src/borg.py
@@ -1,25 +1,35 @@
-# In The Name Of God
-# ========================================
-# [] File Name : borg.py
-#
-# [] Creation Date : 02-06-2015
-#
-# [] Created By : Parham Alvani (parham.alvani@gmail.com)
-# =======================================
-__author__ = 'Parham Alvani'
-
-
+# Borg override __dict__ with its class-level dictionary
+# so all of its instances has access to the same methods and properties.
 class Borg:
-    _namespace = {}
+    __namespace = {"parameter": 10}
 
     def __init__(self):
-        self.__dict__ = Borg._namespace
+        self.__dict__ = Borg.__namespace
 
 
+# set a parameter on b1 (an instance of Borg) and print
+# it, it works as expected.
 b1 = Borg()
-setattr(b1, 'Hello', 'Hi')
-print(b1.Hello)
+setattr(b1, "hello", "Hi")
+print(b1.hello)
+
+
+# define b2 (a new instance of Borg) and it has access to the
+# parameter defined by b1.
 b2 = Borg()
-print(b2.Hello)
-setattr(b2, 'Hello', 'Bye')
-print(b1.Hello)
+print(b2.hello)
+
+# set another value for _hello_ parameter on b2.
+setattr(b2, "hello", "Bye")
+
+# _hello_ parameter is changed on b1.
+print(b1.hello)
+
+# print _paramater_ on b1.
+print(b1.parameter)
+
+# define _name_ parameter on b1.
+b1.name = "Elahe"
+
+# print _name_ parameter from b2.
+print(b2.name)
